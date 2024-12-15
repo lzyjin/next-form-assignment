25a8a217180a9a8dce13858dd8e67126bc75203c
lzyjin <1247y.j@gmail.com>
2024-12-16 00:33:30 +0900

    빌드에러 고침

--- src/components/response/response-menu.tsx
+++ src/components/response/response-menu.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import {EllipsisHorizontalIcon, PencilIcon, TrashIcon} from "@heroicons/react/24/outline";
-import Link from "next/link";
 
 export default function ResponseMenu(
   {toggleMenu, isMenuOpen, openDeleteModal}:
